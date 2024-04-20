49e33068df6d5c8232a0ebd9e4bf2b1f43eac38a
bzarrouki_tr <bzarrouki_tr@vermeg.com>
2024-04-20 20:12:35 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <GoogleOAuthProvider clientId="1048770930095-vtutdfq9alk2go0ss9i2iarvfb6ucudu.apps.googleusercontent.com">
+      <App />
+      </GoogleOAuthProvider>;
+
   </React.StrictMode>
 );
 
